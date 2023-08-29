222c25b378783e1e64f6c93370cdf42a5e5bcf25
Stevehanji <117016798+Stevehanji@users.noreply.github.com>
2023-08-29 18:22:10 +0700

    Add files via upload

---
+++ login_wibu.js
@@ -0,0 +1,34 @@
+var a = document.getElementById("loginBtn")
+var b = document.getElementById("registerBtn")
+var x = document.getElementById("login")
+var y = document.getElementById("register")
+
+function login()
+{
+    x.style.left = "4px";
+    y.style.right = "-520px";
+    a.className += " white-btn";
+    b.className = "btn";
+    x.style.opacity = 1;
+    y.style.opacity = 0;
+}
+
+function register()
+{
+    x.style.left = "-510px";
+    y.style.right = "5px"
+    a.className = "btn";
+    b.className += " white-btn"
+    x.style.opacity = 0;
+    y.style.opacity = 1;
+}
+
+var i = document.getElementById("vanMenu")
+
+if (i.className === "nav-menu")
+{
+    i.className += " responsive"
+} else
+{
+    i.className = "nav-menu"
+}
